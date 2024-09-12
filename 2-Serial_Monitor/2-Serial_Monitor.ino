e4dba9ba828345879f043627bc3f45144f0c78f6
Sanjula Gathsara <sanjulagathsara@gmail.com>
2024-09-12 17:08:29 +0530

    add

---
+++ 2-Serial_Monitor/2-Serial_Monitor.ino
@@ -0,0 +1,22 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  Serial.println(1);
+  delay(1000);
+  Serial.println(2);
+  delay(1000);
+  Serial.println(3);
+  delay(1000);
+  Serial.println(4);
+  delay(1000);
+  Serial.println(5);
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ 3-Count_to_5/3-Count_to_5.ino
@@ -0,0 +1,17 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  for(int i=0;i<=100;i++){
+    Serial.println(i);
+    delay(100);
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
